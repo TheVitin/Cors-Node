f2e75a5ad534e0686fc693246ff69115a06fb081
Vitor Hugo Alves Basso <125588333+TheVitin@users.noreply.github.com>
2023-09-26 13:43:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8008;
+
+// Habilitar o CORS para todas as origens
+app.use(cors());
+
+// Rota de exemplo
+app.get('/', (req, res) => {
+  res.send('Servidor Node.js com CORS habilitado na porta 8000.');
+});
+
+// Iniciar o servidor na porta 8000
+app.listen(PORT, () => {
+  console.log(`Servidor está executando na porta ${PORT}`);
+});
+
